9cfc2c2c63072e32f439c03420b2f32c1de90dea
Tanishq767 <tanishqgr8@gmail.com>
2026-08-23 14:04:49 +0530

    Fix OA upload middleware casing

--- routes/drive.js
+++ routes/drive.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const driveController = require("../controllers/driveController");
 const authenticateRecruiter = require("../middlewares/authenticateRecruiter");
-const oaUpload = require("../middlewares/oaUpload");
+const oaUpload = require("../middlewares/oaupload");
 
 router.get("/", authenticateRecruiter, driveController.getMyDrives);
 
